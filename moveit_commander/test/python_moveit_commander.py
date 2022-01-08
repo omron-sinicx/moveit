5e29fd151fd993b3338f41478a7e430a12218ab9
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2022-01-08 01:00:13 +0100

    Consolidate python tests
    Reduce code duplication by merging testing code for
    non-namespaced and namespaced tests.

--- moveit_commander/test/python_moveit_commander.py
+++ moveit_commander/test/python_moveit_commander.py
@@ -40,14 +40,10 @@ import genpy
 import numpy as np
 import rospy
 import rostest
-import os
+from rosgraph.names import ns_join
 
-from moveit_msgs.msg import (
-    RobotState,
-    PlanningSceneComponents,
-    PlanningScene,
-)
-from sensor_msgs.msg import JointState
+
+from moveit_msgs.msg import RobotState
 
 from moveit_commander import (
     RobotCommander,
@@ -69,7 +65,9 @@ class PythonMoveitCommanderTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.commander = RobotCommander("robot_description")
+        self.commander = RobotCommander(
+            ns_join(PLANNING_NS, "robot_description"), PLANNING_NS
+        )
         self.group = self.commander.get_group(self.PLANNING_GROUP)
 
     @classmethod
@@ -77,29 +75,21 @@ class PythonMoveitCommanderTest(unittest.TestCase):
         pass
 
     def test_enforce_bounds_empty_state(self):
-        in_msg = RobotState()
+        empty_state = RobotState()
         with self.assertRaises(genpy.DeserializationError):
-            self.group.enforce_bounds(in_msg)
+            self.group.enforce_bounds(empty_state)
 
     def test_enforce_bounds(self):
-        in_msg = RobotState()
-        in_msg.joint_state.header.frame_id = "base_link"
-        in_msg.joint_state.name = self.JOINT_NAMES
-        in_msg.joint_state.position = [0] * 6
-        in_msg.joint_state.position[0] = 1000
+        state = RobotState()
+        state.joint_state.header.frame_id = "base_link"
+        state.joint_state.name = ["joint_{}".format(i + 1) for i in range(6)]
+        state.joint_state.position = [0] * 6
+        state.joint_state.position[0] = 1000
 
-        out_msg = self.group.enforce_bounds(in_msg)
+        out_msg = self.group.enforce_bounds(state)
 
-        self.assertEqual(in_msg.joint_state.position[0], 1000)
+        self.assertEqual(state.joint_state.position[0], 1000)
         self.assertLess(out_msg.joint_state.position[0], 1000)
-
-    def test_get_current_state(self):
-        expected_state = RobotState()
-        expected_state.joint_state.header.frame_id = "base_link"
-        expected_state.multi_dof_joint_state.header.frame_id = "base_link"
-        expected_state.joint_state.name = self.JOINT_NAMES
-        expected_state.joint_state.position = [0] * 6
-        self.assertEqual(self.group.get_current_state(), expected_state)
 
     def check_target_setting(self, expect, *args):
         if len(args) == 0:
@@ -157,8 +147,18 @@ class PythonMoveitCommanderTest(unittest.TestCase):
         self.assertTrue(success3)
         self.assertTrue(self.group.execute(plan3))
 
-    def test_gogogo(self):
-        current_joints = np.asarray(self.group.get_current_joint_values())
+
+class PythonMoveitCommanderNoNamespaceTest(PythonMoveitCommanderTest):
+    def test_get_current_state(self):
+        expected_state = RobotState()
+        expected_state.joint_state.header.frame_id = "base_link"
+        expected_state.multi_dof_joint_state.header.frame_id = "base_link"
+        expected_state.joint_state.name = ["joint_{}".format(i + 1) for i in range(6)]
+        expected_state.joint_state.position = [0] * 6
+        self.assertEqual(self.group.get_current_state(), expected_state)
+
+    def test_planning_scene_interface(self):
+        PlanningSceneInterface()
 
         self.group.set_joint_value_target(current_joints)
         self.assertTrue(self.group.go(True))
@@ -220,7 +220,8 @@ if __name__ == "__main__":
     PKGNAME = "moveit_ros_planning_interface"
     NODENAME = "moveit_test_python_moveit_commander"
     rospy.init_node(NODENAME)
-    rostest.rosrun(PKGNAME, NODENAME, PythonMoveitCommanderTest)
-
-    # suppress cleanup segfault
-    os._exit(0)
+    PLANNING_NS = rospy.get_param("~PLANNING_NS", "")
+    if PLANNING_NS:
+        rostest.rosrun(PKGNAME, NODENAME, PythonMoveitCommanderTest)
+    else:
+        rostest.rosrun(PKGNAME, NODENAME, PythonMoveitCommanderNoNamespaceTest)
